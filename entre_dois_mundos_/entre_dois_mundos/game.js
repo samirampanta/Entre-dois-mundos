df7f4e112f1173950392e21dbcce5f191a59835b
Jose1408Ortega <josehenriqueferreiraortega@gmail.com>
2025-06-16 16:06:16 -0300

    Redimensionamento da tela

--- entre_dois_mundos_/entre_dois_mundos/game.js
+++ entre_dois_mundos_/entre_dois_mundos/game.js
@@ -8,17 +8,22 @@ import MundoSombrio2 from './MundoSombrio2.js';
 import Boss from './Boss.js';
 import GameOverScene from './GameOverScene.js';
 
-
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    // Ajuste a largura e altura para preencher a tela
+    width: window.innerWidth,
+    height: window.innerHeight,
     backgroundColor: '#1d1d1d',
+    // Adicione a configuração de escala
+    scale: {
+        mode: Phaser.Scale.FIT, // Ajusta o jogo à tela mantendo a proporção
+        autoCenter: Phaser.Scale.CENTER_BOTH // Centraliza o jogo na tela
+    },
     physics: {
         default: 'arcade',
         arcade: {
             gravity: { y: 600 },
-            debug: true
+            debug: true // Mantenha 'false' na versão final
         }
     },
     scene: [ StartScene, CenaIntro, MundoNormal, MundoNormalScene_1, MundoNormalScene_2, MundoSombrio, MundoSombrio2, Boss, GameOverScene ]
