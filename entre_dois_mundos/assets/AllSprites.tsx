913167ccaaabf013208187ad084e749fb00cefc6
Samira M. Pantaleão <168239105+samirampanta@users.noreply.github.com>
2025-06-17 23:17:00 -0300

    Delete entre_dois_mundos directory

--- entre_dois_mundos/assets/AllSprites.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="AllSprites" tilewidth="16" tileheight="16" tilecount="57600" columns="320">
- <image source="AllSprites.png" width="5120" height="2880"/>
-</tileset>
